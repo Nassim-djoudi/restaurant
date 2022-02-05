1ae9c99c99815a31e433b8a87ccfa12b353ade89
unknown <nassim.djoudi@imt-atlantique.net>
2022-02-05 20:42:30 +0100

    just doing the first section

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 
+import {SubHeading} from "../../components"
+import {images} from "../../constants"
 import './Header.css';
 
 const Header = () => (
-  <div>
-    Header
+  <div classNamê="app__header app__wraper section__padding " id="home">
+    <div className="app__wrapper_info">
+    	<Subheading/>
+    </div>
   </div>
 );
 
